cfa72fdab795add2811ccbead70aee990e5c3222
zhangyunlin <zhangyunlin@ysstech.com>
2018-12-18 16:38:21 +0800

    test remote commit

---
+++ flink-examples/flink-examples-streaming/src/main/scala/com/yss/flink/datastream/SocketWindowWordCount.scala
@@ -0,0 +1,30 @@
+package com.yss.flink.dataStream
+
+import org.apache.flink.api.java.utils.ParameterTool
+import org.apache.flink.streaming.api.scala.StreamExecutionEnvironment
+import org.apache.flink.streaming.api.windowing.time.Time
+
+class SocketWindowWordCount {
+  def main(args: Array[String]): Unit = {
+    var host = "localhost"
+    var port = 10000
+    if (args.length != 0) {
+      val params = ParameterTool.fromArgs(args)
+      host = if (params.get("hostname") != null) params.get("hostname") else "localhost"
+      port = if (params.getInt("port") != null) params.getInt("port") else 10000
+    }
+
+    val env: StreamExecutionEnvironment = StreamExecutionEnvironment.createLocalEnvironment();
+    env.socketTextStream(host, port, '\n')
+      .flatMap(_.split(","))
+      .map((_, 1l))
+      .keyBy(0)
+      .timeWindow(Time.seconds(5))
+      .sum(1)
+      .print()
+      .setParallelism(1)
+
+    env.execute("com.yss.flink.dataStream.SocketWindowWordCount")
+
+  }
+}
